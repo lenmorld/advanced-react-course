f260a8c085b98bd50d188dd858fea1f3322c00c9
lenmorld <lenmorld@gmail.com>
2019-04-04 22:27:36 -0400

    works

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: ['./lib/components/index.js'],
+  entry: ['./lib/renderers/dom.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
@@ -14,7 +14,10 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              ["@babel/transform-runtime"]
+            ]
           }
         }
       }
